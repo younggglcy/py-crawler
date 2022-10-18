b1747b3415d00d2e1dc69ef60fda093f3cb15a34
Young <younggglcy@gmail.com>
2022-10-18 16:00:50 +0800

    perf: request header

--- request.py
+++ request.py
@@ -4,6 +4,7 @@ import random
 from bs4 import BeautifulSoup as bs
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem
+from yaml import safe_load
 
 # inspired by https://www.thepythoncode.com/article/using-proxies-using-requests-in-python
 def get_free_proxies():
@@ -41,30 +42,38 @@ def get_free_proxies():
       continue
   return proxies
 
-def req(**kwargs):
-  try:
-    with Session() as session:
-      proxy = random.choice(get_free_proxies())
-      proxies={
-        'http': proxy['host'],
-      }
-      if proxy['https'] == True:
-        proxies.update({
-          'https': proxy['host']
-        })
-      response = session.get(
-        headers={
-          'User-Agent': get_random_user_agent()
-        }.update(kwargs = kwargs),
-        proxies=proxies,
-        **kwargs,
-      )
-      if response.status_code == 200:
-        return response
-      else:
-        logging.error('request error')
-  except RequestException as e:
-    logging.exception('exception {}'.format(e))
+with open('cfg.yml', 'r') as f:
+  config = safe_load(f)
+  def req(**kwargs):
+    try:
+      with Session() as session:
+        # proxy = random.choice(get_free_proxies())
+        # proxies={
+        #   'http': proxy['host'],
+        # }
+        # if proxy['https'] == True:
+        #   proxies.update({
+        #     'https': proxy['host']
+        #   })
+        response = session.get(
+          headers={
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Connection': 'keep-alive',
+            'Cookie': config['book-cookie'],
+            'Host': 'book.douban.com',
+            'User-Agent': get_random_user_agent()
+          }.update(kwargs = kwargs),
+          # proxies=proxies.update(kwargs = kwargs),
+          **kwargs,
+        )
+        if response and response.status_code == 200:
+          return response
+        else:
+          logging.error('request failed, res: {0}'.format(response))
+    except RequestException as e:
+      logging.exception('exception {}'.format(e))
 
 software_names = [SoftwareName.CHROME.value, SoftwareName.EDGE.value, SoftwareName.FIREFOX.value]
 operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MACOS.value]
